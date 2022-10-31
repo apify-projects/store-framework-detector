4bcf6c0575e853f311c490ba170990cab2dfa403
apify-alexey <alexey.udovydchenko@apify.com>
2022-10-31 13:29:30 +0700

    beta

--- README.md
+++ README.md
@@ -1,28 +1,39 @@
-# Solution template
-This repository serves as a template for creating repositories for new solutions. Using a template makes it easier to create new repos with pre-defined contents and ensures consistency between the repositories.
+# Web Technology Lookup
+Find out what websites are built with. Know your prospects platform before you talk to them. Improve your conversions with validated market adoption.
 
-## How to use this template
+## Input
+Can be any combination of website URLs:
+```
+{
+    "startUrls": [
+        { "url": "https://www.apify.com" }
+    ]
+}
+```
 
-1. Click the Use this template button in the top right corner.
-2. Choose a name for the repository. It should be in the format `vendor`-`customer`-`solution`-`...`. 
-   - **Examples:**
-   - apify-thorn-facebook-scraper
-   - topmonks-microsoft-google-scraper
-   - topmonks-microsoft-google-data-processor
-   - devbros-apple-some-codename-scraper
-3. Make sure the repo is **private**.
-4. Create the repo.
-
-**Once you have the repo created:**
-
-1. Go to Settings -> Manage Access -> Invite teams or people.
-2. Add the **Apify Team** as **admin**. If the solution will be delivered by a partner, add their team as **admin** too.
-4. Edit this README and fill in the details in the template below. If a field cannot be filled, write **N/A**.
-5. Finally, delete this guide from the Readme, so that only the newly added details will remain.
-6. You're done! Thanks for using the template!
-
-# vendor-customer-solution
-
-**Kanban link:** Add link to the Apify Kanban card.
-
-**Issue link:** Add link to the issue created in Delivery Issue Tracker or some other tracking issue.
+## Output
+Framework details, i.e.
+```
+[
+	{
+		"url": "https://www.apify.com/",
+		"name": "Sentry",
+		"description": "Sentry is an open-source platform for workflow productivity, aggregating errors from across the stack in real time.",
+		"confidence": 100,
+		"website": "https://sentry.io/",
+		"categories": [
+			"Issue trackers"
+		]
+	},
+	{
+		"url": "https://www.apify.com/",
+		"name": "Algolia",
+		"description": "Algolia offers a hosted web search product delivering real-time results.",
+		"confidence": 100,
+		"website": "http://www.algolia.com",
+		"categories": [
+			"Search engines"
+		]
+	}
+]
+```
---
+++ src/main.js
@@ -0,0 +1,65 @@
+import { Actor } from 'apify';
+import { KeyValueStore, BasicCrawler, log, Dataset } from 'crawlee';
+import Wappalyzer from 'wappalyzer';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import proxyChain from 'proxy-chain';
+
+Actor.main(async () => {
+    const input = await KeyValueStore.getInput();
+    const {
+        proxyConfig = { useApifyProxy: true },
+        debugLog,
+    } = input;
+
+    if (debugLog) {
+        log.setLevel(log.LEVELS.DEBUG);
+    }
+
+    const proxyConfiguration = await Actor.createProxyConfiguration(proxyConfig);
+
+    const crawler = new BasicCrawler({
+        async requestHandler({ request }) {
+            const { url } = request;
+            try {
+                const proxy = await proxyConfiguration.newUrl();
+                // pptr proxy fix
+                const newProxyUrl = await proxyChain.anonymizeProxy(proxy);
+                const wappalyzer = new Wappalyzer({ proxy: newProxyUrl });
+                await wappalyzer.init();
+
+                // Optionally set additional request headers
+                const headers = {};
+                const site = await wappalyzer.open(url, headers);
+                // Optionally capture and output errors
+                site.on('error', log.error);
+                const results = await site.analyze();
+                const error = Object.values(results.urls)?.[0]?.error;
+                if (error) {
+                    throw new Error(error);
+                }
+                const counter = results?.technologies?.length;
+                log.info(`[DONE]: ${url} with ${counter} results`);
+                await wappalyzer.destroy();
+                if (!counter) {
+                    return;
+                }
+                const saveData = results.technologies.map((x) => {
+                    return {
+                        url,
+                        ...x,
+                        slug: undefined,
+                        icon: undefined,
+                        version: x.version || undefined,
+                        cpe: x.cpe || undefined,
+                        categories: x?.categories?.map((c) => c.name) || undefined,
+                    };
+                });
+                await Dataset.pushData(saveData);
+            } catch (error) {
+                log.error(error);
+            }
+        },
+    });
+
+    await crawler.run(input.startUrls);
+});
